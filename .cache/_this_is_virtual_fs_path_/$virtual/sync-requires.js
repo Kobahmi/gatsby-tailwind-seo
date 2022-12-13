857b49f093c12a86c15d0e4566303d3f688c1567
Nobel <nobel_lamun@hotmail.com>
2022-12-13 16:01:01 -0330

    gatsby installed

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,15 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/home/nobel/work/gatsby-tailwind-seo/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/home/nobel/work/gatsby-tailwind-seo/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/home/nobel/work/gatsby-tailwind-seo/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/home/nobel/work/gatsby-tailwind-seo/src/pages/page-2.js")),
+  "component---src-pages-using-ssr-js": preferDefault(require("/home/nobel/work/gatsby-tailwind-seo/src/pages/using-ssr.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("/home/nobel/work/gatsby-tailwind-seo/src/pages/using-typescript.tsx")),
+  "component---src-templates-using-dsg-js": preferDefault(require("/home/nobel/work/gatsby-tailwind-seo/src/templates/using-dsg.js"))
+}
+
