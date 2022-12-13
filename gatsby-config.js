06ca4c1b862abd5a789343df0da255ab5bedc900
Nobel <nobel_lamun@hotmail.com>
2022-12-13 17:00:28 -0330

    added tailwind CSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-image`,
+    "gatsby-plugin-postcss",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -40,4 +41,4 @@ module.exports = {
       },
     },
   ],
-}
+};
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,11 +5,12 @@
  * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
  */
 
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
-import Header from "./header"
-import "./layout.css"
+import Header from "./header";
+import "./layout.css";
+import { Helmet } from "react-helmet";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -20,32 +21,20 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <>
+      <Helmet>
+        <html lang="en" />
+        <title>Gatsby Tailwind SEO</title>
+        <meta name="description" content="description" />
+      </Helmet>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
 
-export default Layout
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
