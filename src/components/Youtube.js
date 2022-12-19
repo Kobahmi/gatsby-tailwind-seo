2710278943f899c283bb4707baa069f32dc5f1a1
Nobel <nobel_lamun@hotmail.com>
2022-12-19 13:47:58 -0330

    took out animated

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -6,7 +6,9 @@ import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 const Youtube = () => {
   return (
     <section className="flex flex-col justify-center gap-6 text-white bg-slate-900 h-screen">
-      <h1 className="text-lg font-bold text-center sm:text-4xl ">MUST WATCH</h1>
+      <h1 className="text-lg font-bold text-center sm:text-4xl ">
+        MUST WATCH!
+      </h1>
       <div className="w-[680px] mx-auto">
         <LiteYouTubeEmbed
           poster="maxresdefault"
