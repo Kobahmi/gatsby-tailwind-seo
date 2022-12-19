5bea2545be53632acfee77f899f208fb0ac483a1
Nobel <nobel_lamun@hotmail.com>
2022-12-19 13:08:39 -0330

    react lite youtube

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "@justinribeiro/lite-youtube";
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
 const Youtube = () => {
   return (
     <section className="flex flex-col justify-center gap-6 text-white bg-slate-900 h-screen">
       <h1 className="text-lg font-bold text-center sm:text-4xl ">MUST WATCH</h1>
       <div className="w-[680px] mx-auto">
-        <lite-youtube
-          posterquality="maxresdefault"
-          videoid="Uo3cL4nrGOk"
-        ></lite-youtube>
+        <LiteYouTubeEmbed
+          poster="maxresdefault"
+          id="Uo3cL4nrGOk"
+          title="What’s new in Material Design for the web (Chrome Dev Summit 2019)"
+        />
       </div>
     </section>
   );
