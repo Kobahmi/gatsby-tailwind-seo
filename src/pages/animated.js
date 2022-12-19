29331d5caa1e27ce07ab7f9c04edea25d6fc7754
Nobel <nobel_lamun@hotmail.com>
2022-12-19 13:25:33 -0330

    hmtl fix

--- src/pages/animated.js
+++ src/pages/animated.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import Youtube from "../components/Youtube";
-import "@justinribeiro/lite-youtube";
 
 const SecondPage = () => (
   <Layout>
